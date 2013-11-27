bfee334a732ace1fa07ac055c1a37cce4150cfb3
David North <dmhnorth@gmail.com>
2013-11-27 20:58:35 +0000

    working on day 17 ex 4, works with ids. needs an array to store completed tasks

--- PiJ-day-17/ex4/ResUi.java
+++ PiJ-day-17/ex4/ResUi.java
@@ -3,25 +3,43 @@ import java.util.Scanner;
 public class ResUi implements Runnable{
 	//class that counts as soon as created
 	
-	private taskCount = 0;
+	private static int taskCount;
+	private static int taskDuration;
+	private int id;
 
+	public ResUi(int duration) {
+		this.taskDuration = taskDuration;
+		id = taskCount;
+	}
 
 	public static void main(String[] args) {
-		Scanner sc = new scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
 		
 		
-		while (taskCount < 11) {
-			System.out.println("Enter a duration: ");
-			ResUi resui = new Resui(sc.nextInt());
+		while (taskCount < 10) {
+			System.out.println("Enter a duration of " + taskCount + ": ");
+			taskDuration = sc.nextInt();
+			ResUi resui = new ResUi(taskDuration);
 			Thread t = new Thread(resui);
 			t.start();
+
+			taskCount++;
 		}
+		sc.close();
 	}
 		
-		
+	@Override
 	public void run() {
 		
+		try	{
+			Thread.sleep(taskDuration);
+
+		}	catch (InterruptedException ex) {
+			//Does nothing, just speeds things up
+		}
+		//ADD FINISHED TASK TO AN ARRAY
+		System.out.println("Finished tasks: " + (id));
 		
 		//program that asks for inputs until ten have been made
 		

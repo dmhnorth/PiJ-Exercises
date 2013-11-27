ed26bc4f2f2d8e258ea09694273560fd91268271
David North <dmhnorth@gmail.com>
2013-11-27 23:00:47 +0000

    exercise 4 day 17 resui is complete but could do with cleaning up the comma on the end of the list of tasks finished, and could also make it so you stop entering text after ten entries

--- PiJ-day-17/ex4/ResUi.java
+++ PiJ-day-17/ex4/ResUi.java
@@ -1,16 +1,29 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class ResUi implements Runnable{
 	//class that counts as soon as created
 	
 	private static int taskCount;
 	private static int taskDuration;
+	private static ArrayList completeTasks;
 	private int id;
 
 	public ResUi(int duration) {
 		this.taskDuration = taskDuration;
 		id = taskCount;
 	}
+
+	public StringBuilder printCompleteTasks() {
+		StringBuilder completeTasksStr = new StringBuilder();
+		
+		for (int i = 0; i < completeTasks.size(); i++) {
+			if (!completeTasks.get(i).equals(0) && completeTasks != null) {
+				completeTasksStr.append(completeTasks.get(i) + ", ");	//could reorganise as a sout
+			}
+		} return completeTasksStr;
+	} 
+	
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -38,8 +51,16 @@ public class ResUi implements Runnable{
 		}	catch (InterruptedException ex) {
 			//Does nothing, just speeds things up
 		}
-		//ADD FINISHED TASK TO AN ARRAY
-		System.out.println("Finished tasks: " + (id));
+		//ARRAY FOR COMPLETE TASKS
+		if (completeTasks == null) {
+			completeTasks = new ArrayList();
+		}
+		completeTasks.add(id);
+		
+
+
+		System.out.println("Finished task (singleID): " + (id));
+		System.out.println("List of finished tasks (ArrayList): " + printCompleteTasks());
 		
 		//program that asks for inputs until ten have been made
 		

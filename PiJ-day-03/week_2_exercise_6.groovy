25511479d4359ede254653d3579d12f165c8f4d5
David North <dmhnorth@gmail.com>
2013-12-01 17:03:04 +0000

    day 3 exercise 6 is completed

--- PiJ-day-03/week_2_exercise_6.groovy
+++ PiJ-day-03/week_2_exercise_6.groovy
@@ -1,14 +1,11 @@
-//only working for single letters at the moment
+//works!
 //Write a program that reads some text from the user and then says how many ent2ters ’e’ are there in that text. Then modify the program so that it reads the text from the user and then asks for a ent2ter. The program should then say how many times you can find the ent2ter in the text.
 
 
 String ent2 = "";
 String ent1 = "";
-String ent1Chk = "";
 String ent2Chk = "";
-String store = "";
-int chcnt;
-
+int chcnt
 
 
 
@@ -17,22 +14,33 @@ ent1 = System.console().readLine()
 println "Now enter the string you would like to find occurence of in string one: "
 ent2 = System.console().readLine()
 
-for (int i = 0; i < ent2.length(); i++) {
-	char x = ent2.charAt(i);
-	store = ent2Chk;
-	ent2Chk = ent2Chk + (ent2.charAt(i)); //check whether next letter is same before adding
 
-	if (ent2Chk == ent1) {
+
+for (int i = 0; i < ent2.length(); i++) {
+	//System.out.println("i is currently: " + i);
+
+	try {
+	if (ent2.substring(i, i + ent1.length()) == ent1) {
+		chcnt++;
+		System.out.println(ent2.substring(i, ent1.length()) + " matched: " + chcnt + " times so far.")
+		
+	} else {
+		//System.out.println(ent2.substring(i, ent1.length()) + " doesn't match.")
+	}
+	} catch (Exception ex) {
+		System.out.println("...just catching out of bounds checks...");
+	}
+/*
+	if (ent2Chk == ent1) {	//final check for a string, works
 		
 		chcnt++;
 		System.out.println("ent2Chk matched: " + ent2Chk + " " + chcnt + " times so far.")
 		ent2Chk = "";
-		store = "";
-		}
+			} 
 
-		//needs to clear out chk2 because its just filling up with junk between checks
 	}
-	if (ent2Chk == ent1) {	//compare strings
-		chcnt++;
+	
+*/
+
 	}
 	println "There are " + chcnt + " occurences of the string: " + ent1 + ", in the string: " + ent2

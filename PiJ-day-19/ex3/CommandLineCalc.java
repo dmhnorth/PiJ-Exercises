6b0d1032c4bb40e316ea58d840f835f35cfc124b
David North <dmhnorth@gmail.com>
2013-12-04 22:09:14 +0000

    ex3 day 19 has had some work so it now works well. Would be nice to fix it if no symbols are used just numbers, or would be nice to fix the method that the strings are created with from String to StringBuilder

--- PiJ-day-19/ex3/CommandLineCalc.java
+++ PiJ-day-19/ex3/CommandLineCalc.java
@@ -4,7 +4,7 @@ public class CommandLineCalc {
 	public String calculate(String userEntry) {
 	
 		String num1str = "";
-		int num1 = 0, num2 = 0;
+		double num1 = 0, num2;
 
 
                                //change to StringBuffer
@@ -19,12 +19,12 @@ public class CommandLineCalc {
 					//this is purely an exception so spaces are ignored
 			} else
 			{
-					num1 = Integer.parseInt(num1str);
+					num1 = Double.parseDouble(num1str);
 					num1str = "";
 			}
 
 		}
-	num2 = Integer.parseInt(num1str);
+	num2 = Double.parseDouble(num1str);
 
 	String answer = "End";
 
